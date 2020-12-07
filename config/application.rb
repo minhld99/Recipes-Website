e906e52ae51c623e4170c8a1a31cd84c9122f61d
minhld99 <50240885+minhld99@users.noreply.github.com>
2020-12-08 01:17:05 +0700

    Config browser_list for .babelrc and reconfig Rails generators

--- config/application.rb
+++ config/application.rb
@@ -33,3 +33,11 @@ module RecipesWebsite
     config.generators.system_tests = nil
   end
 end
+
+config.generators do |g|
+  g.test_framework  false
+  g.stylesheets     false
+  g.javascripts     false
+  g.helper          false
+  g.channel         assets: false
+end
