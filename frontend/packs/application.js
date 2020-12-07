a4788a27cd52cb20066fc330119fa4dcfc579fe8
minhld99 <50240885+minhld99@users.noreply.github.com>
2020-12-08 02:37:34 +0700

    Add first component (HomePage)

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -3,9 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "./application.css";
-
-document.body.insertAdjacentHTML("afterbegin", "Amazing gút chóp em!");
+import "init";
+import "components/StaticPages/page";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
