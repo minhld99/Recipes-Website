72c174b460ad82a12fbde99bfe8f89b1389248b4
minhld99 <50240885+minhld99@users.noreply.github.com>
2020-12-08 01:34:26 +0700

    Separate frontend & backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    
+    # Connect to views
+    prepend_view_path Rails.root.join("frontend")
 end
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -3,10 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
+import "./application.css";
 
+document.body.insertAdjacentHTML("afterbegin", "Amazing gút chóp em!");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
