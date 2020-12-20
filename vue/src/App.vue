1398d6114dfaf69eb255bc5df40822f530933769
thanhha0426 <nguyenthanhha041999@gmail.com>
2020-12-20 23:07:25 +0700

    Add App.vue & routes.js

---
+++ vue/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+      <div class="navbar-menu">
+        <div class="navbar-start">
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div v-if="isAuthenticated" class="buttons">
+              <router-link :to="{name: 'profile', params: {id: userId}}" v-text="userName" class="button is-text">
+              </router-link>
+              <button class="button is-outlined" @click="logout">
+                Log out
+              </button>
+            </div>
+
+            <div v-else class="buttons">
+              <router-link to="/register" class="button is-success">
+                Sign up
+              </router-link>
+              <router-link to="/login" class="button is-outlined">
+                Log in
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-6 is-offset-3">
+              <b-message title="Danger" type="is-danger" v-if="getErrors" @close="close">
+                {{ getErrors }}
+              </b-message>
+            </div>
+          </div>
+          <router-view></router-view>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+
+  methods: {
+    logout() {
+      this.$store.commit('logout')
+      this.$router.push('/login')
+    },
+
+    close() {
+      this.$store.commit('setErrors', null);
+    }
+  },
+
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters.isAuthenticated;
+    },
+
+    userId() {
+      return this.$store.getters.getUserId;
+    },
+
+    userName() {
+      return this.$store.getters.getUserName;
+    },
+
+    getErrors() {
+      return this.$store.getters.getErrors;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/routes.js
@@ -0,0 +1,29 @@
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+
+import Profile from './components/user/Profile'
+
+import store from './store'
+
+const ifNotAuthenticated = (to, from, next) => {
+    if (!store.getters.isAuthenticated) {
+        next()
+        return
+    }
+    // next('/recipes')
+}
+
+const ifAuthenticated = (to, from, next) => {
+    if (store.getters.isAuthenticated) {
+        next()
+        return
+    }
+    next('/login')
+}
+
+export const routes = [
+
+    { path: '/register', component: Register, beforeEnter: ifNotAuthenticated },
+    { path: '/login', component: Login, beforeEnter: ifNotAuthenticated },
+    { path: '/users/profile/:id', component: Profile, name: 'profile', beforeEnter: ifAuthenticated },
+];
