c66c3ada7a192c023f9c931b59bf09dcfcb375b8
minhld99 <50240885+minhld99@users.noreply.github.com>
2020-12-24 00:08:51 +0700

    Redefine db structure, add sample data

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password validations: false
 
+  validates :full_name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true
   validates :password_confirmation, presence: true
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -2,23 +2,37 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_035129) do
+ActiveRecord::Schema.define(version: 2020_12_23_154704) do
 
-  create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "follows", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+    t.index ["following_id", "follower_id"], name: "index_follows_on_following_id_and_follower_id", unique: true
+    t.index ["following_id"], name: "index_follows_on_following_id"
+  end
+
+  create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "category", null: false
+    t.text "description", null: false
+    t.string "category", null: false
     t.float "preparation_time", null: false
-    t.float "oven_time", null: false
+    t.float "cooking_time", null: false
     t.text "ingredients", null: false
-    t.text "steps", null: false
+    t.text "instructions", null: false
+    t.integer "serving", null: false
+    t.string "dificulty", null: false
+    t.boolean "vegetarian"
     t.string "image"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -26,7 +40,16 @@ ActiveRecord::Schema.define(version: 2018_09_27_035129) do
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "saved_recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "recipe_id"
+    t.boolean "favorited"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "full_name", null: false
     t.string "name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
