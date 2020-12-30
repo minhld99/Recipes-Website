22a254990d99f7cf11dc32bb4e86b1854a7caffa
minhld99 <50240885+minhld99@users.noreply.github.com>
2020-12-30 20:28:07 +0700

    Active storage install, migration and schema updated

--- rails/app/controllers/api/v1/users_controller.rb
+++ rails/app/controllers/api/v1/users_controller.rb
@@ -1,28 +1,24 @@
-module Api
-  module V1
-    class UsersController < ApplicationController
-      skip_before_action :authenticate_request, :except => [:show]
+class Api::V1::UsersController < ApplicationController
+  skip_before_action :authenticate_request, :except => [:show]
 
-      def show
-        user = User.find(params[:id])
-        data = {:user => user, :recipes => user.recipes}
-        render json: data, status: :ok
-      end
+  def show
+    user = User.find(params[:id])
+    data = {:user => user, :recipes => user.recipes}
+    render json: data, status: :ok
+  end
 
-      def create
-        user = User.new(user_params)
-        if user.save
-          render json: {data: user}, status: :ok
-        else
-          render json: {errors: user.errors}, status: :unprocessable_entity
-        end
-      end
-
-      private
-
-      def user_params
-        params.permit(:full_name, :name, :email, :password, :password_confirmation)
-      end
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: {data: user}, status: :ok
+    else
+      render json: {errors: user.errors}, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def user_params
+    params.permit(:full_name, :name, :email, :password, :password_confirmation)
   end
 end
