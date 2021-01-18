966287cd4e5663aeb5487e9868d8c4dc7fb134ba
minhld99 <50240885+minhld99@users.noreply.github.com>
2021-01-19 06:44:05 +0700

    Update user api, add profile

--- rails/app/controllers/api/v1/users_controller.rb
+++ rails/app/controllers/api/v1/users_controller.rb
@@ -3,14 +3,23 @@ class Api::V1::UsersController < ApplicationController
 
   def show
     user = User.find(params[:id])
-    data = {:user => user, :recipes => user.recipes}
-    render json: data, status: :ok
+    # data = {:user => user, :recipes => user.recipes}
+    render json: user, status: :ok
   end
 
   def create
     user = User.new(user_params)
     if user.save
       render json: {data: user}, status: :ok
+    else
+      render json: {errors: user.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      render json: user, status: :ok
     else
       render json: {errors: user.errors}, status: :unprocessable_entity
     end
